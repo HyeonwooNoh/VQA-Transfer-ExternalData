f76ff47a335652100d378dc80b9b766d30b850b8
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2018-02-23 16:54:13 -0800

    vocab construction

---
+++ data/tools/tools.py
@@ -0,0 +1,12 @@
+def clean_description(string):
+    string = string.lower()
+    string = string.replace('\n', '')
+    string = string.replace(' re', ' are')
+    string = string.replace('.', ' .')  # '  ' to ' ' should follow this.
+    string = string.replace('   ', ' ')
+    string = string.replace('  ', ' ')
+    if string[-1] == ' ':
+        string = string[:-1]
+    if string[0] == ' ':
+        string = string[1:]
+    return string
