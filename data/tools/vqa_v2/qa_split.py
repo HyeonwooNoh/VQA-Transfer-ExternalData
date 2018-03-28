8a5105965863f53a8f823df2906684662383b8e1
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2018-03-28 18:41:26 +0900

    vqa qa_split function is updated

--- data/tools/vqa_v2/qa_split.py
+++ data/tools/vqa_v2/qa_split.py
@@ -41,7 +41,7 @@ parser.add_argument('--occ_thres_2', type=int, default=50,
                     'occurrence is smaller, they are splited into train-reserve '
                     'and test.')
 parser.add_argument('--save_split_dir', type=str,
-                    default='data/preprocessed/vqa_v2/qa_split')
+                    default='data/preprocessed/vqa_v2/new_qa_split')
 config = parser.parse_args()
 
 vocab = json.load(open(config.vocab_path, 'r'))
@@ -100,16 +100,117 @@ qid2anno = {a['question_id']: a for a in merge_annotations}
 for q in tqdm(merge_questions, desc='merge question and annotations'):
     qid2anno[q['question_id']]['question'] = q['question']
 
+contractions = {
+    "aint": "ain't", "arent": "aren't", "cant": "can't", "couldve":
+    "could've", "couldnt": "couldn't", "couldn'tve": "couldn't've",
+    "couldnt've": "couldn't've", "didnt": "didn't", "doesnt":
+    "doesn't", "dont": "don't", "hadnt": "hadn't", "hadnt've":
+    "hadn't've", "hadn'tve": "hadn't've", "hasnt": "hasn't", "havent":
+    "haven't", "hed": "he'd", "hed've": "he'd've", "he'dve":
+    "he'd've", "hes": "he's", "howd": "how'd", "howll": "how'll",
+    "hows": "how's", "Id've": "I'd've", "I'dve": "I'd've", "Im":
+    "I'm", "Ive": "I've", "isnt": "isn't", "itd": "it'd", "itd've":
+    "it'd've", "it'dve": "it'd've", "itll": "it'll", "let's": "let's",
+    "maam": "ma'am", "mightnt": "mightn't", "mightnt've":
+    "mightn't've", "mightn'tve": "mightn't've", "mightve": "might've",
+    "mustnt": "mustn't", "mustve": "must've", "neednt": "needn't",
+    "notve": "not've", "oclock": "o'clock", "oughtnt": "oughtn't",
+    "ow's'at": "'ow's'at", "'ows'at": "'ow's'at", "'ow'sat":
+    "'ow's'at", "shant": "shan't", "shed've": "she'd've", "she'dve":
+    "she'd've", "she's": "she's", "shouldve": "should've", "shouldnt":
+    "shouldn't", "shouldnt've": "shouldn't've", "shouldn'tve":
+    "shouldn't've", "somebody'd": "somebodyd", "somebodyd've":
+    "somebody'd've", "somebody'dve": "somebody'd've", "somebodyll":
+    "somebody'll", "somebodys": "somebody's", "someoned": "someone'd",
+    "someoned've": "someone'd've", "someone'dve": "someone'd've",
+    "someonell": "someone'll", "someones": "someone's", "somethingd":
+    "something'd", "somethingd've": "something'd've", "something'dve":
+    "something'd've", "somethingll": "something'll", "thats":
+    "that's", "thered": "there'd", "thered've": "there'd've",
+    "there'dve": "there'd've", "therere": "there're", "theres":
+    "there's", "theyd": "they'd", "theyd've": "they'd've", "they'dve":
+    "they'd've", "theyll": "they'll", "theyre": "they're", "theyve":
+    "they've", "twas": "'twas", "wasnt": "wasn't", "wed've":
+    "we'd've", "we'dve": "we'd've", "weve": "we've", "werent":
+    "weren't", "whatll": "what'll", "whatre": "what're", "whats":
+    "what's", "whatve": "what've", "whens": "when's", "whered":
+    "where'd", "wheres": "where's", "whereve": "where've", "whod":
+    "who'd", "whod've": "who'd've", "who'dve": "who'd've", "wholl":
+    "who'll", "whos": "who's", "whove": "who've", "whyll": "why'll",
+    "whyre": "why're", "whys": "why's", "wont": "won't", "wouldve":
+    "would've", "wouldnt": "wouldn't", "wouldnt've": "wouldn't've",
+    "wouldn'tve": "wouldn't've", "yall": "y'all", "yall'll":
+    "y'all'll", "y'allll": "y'all'll", "yall'd've": "y'all'd've",
+    "y'alld've": "y'all'd've", "y'all'dve": "y'all'd've", "youd":
+    "you'd", "youd've": "you'd've", "you'dve": "you'd've", "youll":
+    "you'll", "youre": "you're", "youve": "you've"
+}
+
+manual_map = { 'none': '0',
+              'zero': '0',
+              'one': '1',
+              'two': '2',
+              'three': '3',
+              'four': '4',
+              'five': '5',
+              'six': '6',
+              'seven': '7',
+              'eight': '8',
+               'nine': '9',
+              'ten': '10'}
+articles = ['a', 'an', 'the']
+period_strip = re.compile("(?!<=\d)(\.)(?!\d)")
+comma_strip = re.compile("(\d)(\,)(\d)")
+punct = [';', r"/", '[', ']', '"', '{', '}',
+                '(', ')', '=', '+', '\\', '_', '-',
+                '>', '<', '@', '`', ',', '?', '!']
 
 def split_with_punctuation(string):
     return re.findall(r"'s+|[\w]+|[.,!?;-]", string)
+
+def process_punctuation(inText):
+    outText = inText
+    for p in punct:
+        if (p + ' ' in inText or ' ' + p in inText) \
+           or (re.search(comma_strip, inText) != None):
+            outText = outText.replace(p, '')
+        else:
+            outText = outText.replace(p, ' ')
+    outText = period_strip.sub("", outText, re.UNICODE)
+    return outText
+
+def process_digit_article(inText):
+    outText = []
+    tempText = inText.lower().split()
+    for word in tempText:
+        word = manual_map.setdefault(word, word)
+        if word not in articles:
+            outText.append(word)
+        else:
+            pass
+    for wordId, word in enumerate(outText):
+        if word in contractions:
+            outText[wordId] = contractions[word]
+    outText = ' '.join(outText)
+    return outText
+
+def preprocess_answer(answer):
+    answer = process_digit_article(process_punctuation(answer))
+    answer = answer.replace(',', '')
+    return answer
 
 for qid in tqdm(qid2anno.keys(), desc='tokenize QA'):
     anno = qid2anno[qid]
     qid2anno[qid]['q_tokens'] = split_with_punctuation(
         anno['question'].lower())
     qid2anno[qid]['a_tokens'] = split_with_punctuation(
-        anno['multiple_choice_answer'].lower())
+        preprocess_answer(anno['multiple_choice_answer'].lower()))
+    processed_answers = []
+    for answer in anno['answers']:
+        a_tokens = split_with_punctuation(
+            preprocess_answer(answer['answer'].lower()))
+        processed_answers.append(' '.join(a_tokens))
+    qid2anno[qid]['processed_answers'] = processed_answers
 
 """
 Test question or answer could have training object words, but it should contain
