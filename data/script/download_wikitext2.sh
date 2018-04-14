0d713bb0d021e6fe90adc1fbf2d21cd7f1b20dcf
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2018-04-15 01:40:20 +0900

    download wikitext2

---
+++ data/script/download_wikitext2.sh
@@ -0,0 +1,6 @@
+mkdir wikitext2
+cd wikitext2
+wget https://raw.githubusercontent.com/pytorch/examples/master/word_language_model/data/wikitext-2/train.txt
+wget https://raw.githubusercontent.com/pytorch/examples/master/word_language_model/data/wikitext-2/valid.txt
+wget https://raw.githubusercontent.com/pytorch/examples/master/word_language_model/data/wikitext-2/test.txt
+cd ..
