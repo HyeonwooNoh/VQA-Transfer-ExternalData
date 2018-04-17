0c13c356fac7e721d4f7f9a92a57865dbbd901fe
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2018-04-17 12:14:07 +0900

    vlmap_bf_wordset model is added

--- vlmap_memft/trainer.py
+++ vlmap_memft/trainer.py
@@ -18,6 +18,8 @@ class Trainer(object):
             from vlmap_memft.model_vlmap import Model
         elif model_type == 'vlmap_bf_only':
             from vlmap_memft.model_vlmap_bf_only import Model
+        elif model_type == 'vlmap_bf_wordset':
+            from vlmap_memft.model_vlmap_bf_wordset import Model
         elif model_type == 'vlmap_autoenc':
             from vlmap_memft.model_vlmap_autoenc import Model
         elif model_type == 'vlmap_autoenc_full':
@@ -304,7 +306,7 @@ def main():
     parser.add_argument('--batch_size', type=int, default=512, help=' ')
     parser.add_argument('--model_type', type=str, default='vlmap', help=' ',
                         choices=['vlmap', 'vlmap_bf_only', 'vlmap_autoenc',
-                                 'vlmap_autoenc_full'])
+                                 'vlmap_autoenc_full', 'vlmap_bf_wordset'])
     config = parser.parse_args()
     check_config(config)
 
