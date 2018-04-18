8fc87426f7f8d0a59a662d2079f2aa1587ca7b6f
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2018-04-19 00:12:02 +0900

    vlmap wordset only withatt is added

--- vlmap_memft/trainer.py
+++ vlmap_memft/trainer.py
@@ -20,6 +20,8 @@ class Trainer(object):
             from vlmap_memft.model_vlmap_wordset import Model
         elif model_type == 'vlmap_wordset_only':
             from vlmap_memft.model_vlmap_wordset_only import Model
+        elif model_type == 'vlmap_wordset_only_withatt':
+            from vlmap_memft.model_vlmap_wordset_only_withatt import Model
         elif model_type == 'vlmap_bf_only':
             from vlmap_memft.model_vlmap_bf_only import Model
         elif model_type == 'vlmap_bf_wordset':
@@ -312,7 +314,7 @@ def main():
     parser.add_argument('--batch_size', type=int, default=512, help=' ')
     parser.add_argument('--model_type', type=str, default='vlmap', help=' ',
                         choices=['vlmap', 'vlmap_wordset', 'vlmap_wordset_only',
-                                 'vlmap_bf_only',
+                                 'vlmap_wordset_only_withatt', 'vlmap_bf_only',
                                  'vlmap_autoenc', 'vlmap_bf_or_wordset',
                                  'vlmap_autoenc_full', 'vlmap_bf_wordset'])
     config = parser.parse_args()
