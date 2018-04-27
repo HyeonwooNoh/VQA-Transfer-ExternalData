6c8fe81accd07ffac01c40b9ff6d715ac017d610
Taehoon Kim <carpedm20@gmail.com>
2018-04-27 15:21:43 +0900

    chnage name of bookcorpus/*.py

--- data/script/download_bookcorpus.sh
+++ data/script/download_bookcorpus.sh
@@ -2,5 +2,5 @@
 mkdir BookCorpus
 cd BookCorpus
 wget --user mbweb --password Tiwp49mS3Ts http://www.cs.toronto.edu/~mbweb/data/books_in_sentences.tar
-unzip books_in_sentences.tar
-rm books_in_sentences.tar
+tar -zvf books_in_sentences.tar
+#rm books_in_sentences.tar
