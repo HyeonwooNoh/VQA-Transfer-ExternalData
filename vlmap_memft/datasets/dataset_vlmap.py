bca33f6ef50e6cbe62b21d33479ac29f40d545dc
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2018-04-28 11:27:17 +0900

    dataset vlmap - wordset and enwiki shuffle

--- vlmap_memft/datasets/dataset_vlmap.py
+++ vlmap_memft/datasets/dataset_vlmap.py
@@ -73,16 +73,17 @@ class Dataset(object):
             log.warn('loading {} features done ..'.format(split))
 
         self.wordset_choice_idx = {
-                'obj_label': defaultdict(lambda: defaultdict(int)),
-                'obj_fill': defaultdict(lambda: defaultdict(int)),
-                'attr_label': defaultdict(lambda: defaultdict(int)),
-                'attr_fill': defaultdict(lambda: defaultdict(int)),
+            'obj_label': defaultdict(lambda: defaultdict(int)),
+            'obj_fill': defaultdict(lambda: defaultdict(int)),
+            'attr_label': defaultdict(lambda: defaultdict(int)),
+            'attr_fill': defaultdict(lambda: defaultdict(int)),
+        }
 
-        self.enwiki_choice_idx = \
-                'obj_label': defaultdict(lambda: defaultdict(int)),
-                'obj_fill': defaultdict(lambda: defaultdict(int)),
-                'attr_label': defaultdict(lambda: defaultdict(int)),
-                'attr_fill': defaultdict(lambda: defaultdict(int)),
+        self.enwiki_choice_idx = {
+            'obj_label': defaultdict(lambda: defaultdict(int)),
+            'obj_fill': defaultdict(lambda: defaultdict(int)),
+            'attr_label': defaultdict(lambda: defaultdict(int)),
+            'attr_fill': defaultdict(lambda: defaultdict(int)),
         }
 
         log.info('dataset {} {} init done'.format(name, split))
@@ -97,41 +98,31 @@ class Dataset(object):
         config.max_box_num = self.max_box_num
         return config
 
-
-    def sample_wordset_and_context_idx(
-            self, category, task, image_idx, idx)
+    def sample_wordset_and_context_idx(self, e, category, task, image_idx, idx):
         # category: obj, attr, task: label, fill
-        
-        wordsets = self.ws_dict['ans2wordset'][e[task]]
-        enwiki_context_idxs = \
-                self.enwiki_dict['ans2context_idx'][e[task]]
-        enwiki_context_probs = \
-                self.enwiki_dict['ans2context_prob'][e[task]]
 
-        if True:
-            wordset = RANDOM_STATE.choice(wordsets)
-            enwiki_context_idx = RANDOM_STATE.choice(
-                context_idxs, p=context_probs)
-        else:
-            wordset_choice_idx = \
-                    self.wordset_choice_idx["{}_{}".format(category, task)]
-            enwiki_choice_idx = \
-                    self.enwiki_choice_idx["{}_{}".format(category, task)]
+        wordsets = self.ws_dict['ans2shuffled_wordset'][e[task]]
+        enwiki_context_idxs = self.enwiki_dict['ans2shuffled_context_idx'][e[task]]
 
-            wordset_choice_idxs = \
-                    wordset_choice_idx[image_id][idx]
-            enwiki_choice_idxs = \
-                    enwiki_choice_idx[image_id][idx]
+        wordset_choice_idx = \
+            self.wordset_choice_idx["{}_{}".format(category, task)]
+        enwiki_choice_idx = \
+            self.enwiki_choice_idx["{}_{}".format(category, task)]
 
-            wordset = wordsets[
-                    wordset_choice_idxs % len(wordset_choice_idxs)]
-            enwiki_context_idx = enwiki_context_idxs[
-                    enwiki_choice_idxs % len(enwiki_choice_idxs)]
-            enwiki_context_prob = enwiki_context_probs[
-                    enwiki_choice_idxs % len(enwiki_choice_idxs)]
+        wordset_choice_idxs = wordset_choice_idx[image_idx][idx]
+        enwiki_choice_idxs = enwiki_choice_idx[image_idx][idx]
 
-            wordset_choice_idx[image_id][idx] += 1
-            enwiki_choice_idx[image_id][idx] += 1
+        wordset = wordsets[
+            wordset_choice_idxs % len(wordsets)]
+        enwiki_context_idx = enwiki_context_idxs[
+            enwiki_choice_idxs % len(enwiki_context_idxs)]
+
+        wordset_choice_idx[image_idx][idx] += 1
+        if wordset_choice_idx[image_idx][idx] >= len(wordsets):
+            wordset_choice_idx[image_idx][idx] = 0
+        enwiki_choice_idx[image_idx][idx] += 1
+        if enwiki_choice_idx[image_idx][idx] >= len(enwiki_context_idxs):
+            enwiki_choice_idx[image_idx][idx] = 0
 
         return wordset, enwiki_context_idx
 
@@ -169,10 +160,10 @@ class Dataset(object):
             weights.append(weight)
             labels.append(e['label'])
             normal_boxes.append(e['normal_box'])
-            
+
             wordset, enwiki_context_idx = \
-                    self.sample_wordset_and_context_idx(
-                            'obj', 'label', image_idx, idx)
+                self.sample_wordset_and_context_idx(
+                    e, 'obj', 'label', image_idx, idx)
             wordsets.append(wordset)
             enwiki_context_idx_list.append(enwiki_context_idx)
 
@@ -218,28 +209,6 @@ class Dataset(object):
                 rand_attribute_labels.append(rand_label)
                 rand_wordset = RANDOM_STATE.choice(
                     self.ws_dict['ans2wordset'][rand_label])
-            else:
-                category, task = 'attr', 'label'
-                wordset_choice_idx = \
-                        self.wordset_choice_idx["{}_{}".format(category, task)]
-                enwiki_choice_idx = \
-                        self.enwiki_choice_idx["{}_{}".format(category, task)]
-
-                wordset_choice_idxs = \
-                        wordset_choice_idx[image_id][idx]
-                enwiki_choice_idxs = \
-                        enwiki_choice_idx[image_id][idx]
-
-                random_label = e[
-                        wordset_choice_idxs % len(wordset_choice_idxs)]
-                rand_attribute_labels.append(rand_label)
-
-                wordsets = self.ws_dict['ans2wordset'][rand_label]
-                rand_wordset = wordsets[
-                        enwiki_choice_idxs % len(enwiki_choice_idxs)]
-
-                wordset_choice_idx[image_id][idx] += 1
-                enwiki_choice_idx[image_id][idx] += 1
 
             rand_wordsets.append(rand_wordset)
             wordset_labels = list(
@@ -285,8 +254,8 @@ class Dataset(object):
             fills.append(e['fill'])
 
             wordset, enwiki_context_idx = \
-                    self.sample_wordset_and_context_idx(
-                            'obj', 'fill', image_idx, idx)
+                self.sample_wordset_and_context_idx(
+                    e, 'obj', 'fill', image_idx, idx)
             wordsets.append(wordset)
             enwiki_context_idx_list.append(enwiki_context_idx)
 
@@ -332,8 +301,8 @@ class Dataset(object):
             fills.append(e['fill'])
 
             wordset, enwiki_context_idx = \
-                    self.sample_wordset_and_context_idx(
-                            'attr', 'fill', image_idx, idx)
+                self.sample_wordset_and_context_idx(
+                    e, 'attr', 'fill', image_idx, idx)
             wordsets.append(wordset)
             enwiki_context_idx_list.append(enwiki_context_idx)
 
